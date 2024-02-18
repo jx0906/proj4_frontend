2bb2f814d2cd3fecbc70eeeff131e0de016814c8
jx0906 <sandy_hong@live.com>
2024-02-18 21:10:49 +0800

    update theme

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@
 wrapped in various context providers such as react, router and mantine */
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { MantineProvider } from "@mantine/core";
+import { createTheme, MantineProvider } from "@mantine/core";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
@@ -12,19 +12,19 @@ import "@mantine/core/styles.css";
 import "@mantine/notifications/styles.css";
 import "@mantine/dates/styles.css";
 
-// // https://mantine.dev/theming/default-theme/
-// const theme = createTheme({
-//   fontFamily: "Montserrat, sans-serif",
-//   defaultRadius: "md",
-//   cursorType: "pointer",
-//   primaryColor: "yellow",
-// });
+// https://mantine.dev/theming/default-theme/
+const theme = createTheme({
+  fontFamily: "Montserrat, sans-serif",
+  defaultRadius: "md",
+  cursorType: "pointer",
+  //https://mantine.dev/colors-generator/?color=C91A52
+  primaryColor: "pale red",
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
-      <MantineProvider>
-        {/* theme={theme} defaultColorScheme="light" */}
+      <MantineProvider theme={theme} defaultColorScheme="light">
         <App />
       </MantineProvider>
     </Router>
