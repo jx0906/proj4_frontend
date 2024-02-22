ecfbc6e17f5a776436ae50760a31ee696d140576
jx0906 <sandy_hong@live.com>
2024-02-22 17:34:59 +0800

    enhance user action for sharing URL

--- src/components/parts/UserActions.jsx
+++ src/components/parts/UserActions.jsx
@@ -1,28 +1,58 @@
-import { ActionIcon, rem } from "@mantine/core";
+import { ActionIcon, rem, Tooltip } from "@mantine/core";
 import {
   IconBookmarkPlus,
   IconCirclePlus,
   IconShare,
   IconEdit,
 } from "@tabler/icons-react";
+import { notifications } from "@mantine/notifications";
 
 export default function UserActions() {
+  function handleClickToShare(evt) {
+    evt.preventDefault();
+    // Get the URL field
+    var getURL = window.location.href;
+    // Select the text field
+    // copyText.select();
+    // copyText.setSelectionRange(0, 99999); // For mobile devices
+
+    // Copy the text inside the text field
+    navigator.clipboard.writeText(getURL.value);
+
+    // Alert the copied text
+    notifications.show({
+      message: "URL copied!",
+      autoClose: 1000,
+    });
+  }
+
   return (
     <ActionIcon.Group>
-      <ActionIcon variant="default" size="lg" aria-label="Gallery">
-        <IconBookmarkPlus style={{ width: rem(20) }} stroke={1.5} />
-      </ActionIcon>
-      <ActionIcon variant="default" size="lg" aria-label="Settings">
-        <IconShare style={{ width: rem(20) }} stroke={1.5} />
-      </ActionIcon>
+      <Tooltip label="Bookmark to Collection">
+        <ActionIcon variant="default" size="lg">
+          <IconBookmarkPlus style={{ width: rem(20) }} stroke={1.5} />
+        </ActionIcon>
+      </Tooltip>
+
+      <Tooltip label="Copy URL for sharing">
+        <ActionIcon variant="default" size="lg" onClick={handleClickToShare}>
+          <IconShare style={{ width: rem(20) }} stroke={1.5} />
+        </ActionIcon>
+      </Tooltip>
+
       {/* if user is logged on and creator of recipe - can edit */}
-      <ActionIcon variant="default" size="lg" aria-label="Likes">
-        <IconEdit style={{ width: rem(20) }} stroke={1.5} />
-      </ActionIcon>
+      <Tooltip label="Edit Recipe">
+        <ActionIcon variant="default" size="lg">
+          <IconEdit style={{ width: rem(20) }} stroke={1.5} />
+        </ActionIcon>
+      </Tooltip>
+
       {/* if user is logged on - can add notes */}
-      <ActionIcon variant="default" size="lg" aria-label="Likes">
-        <IconCirclePlus style={{ width: rem(20) }} stroke={1.5} />
-      </ActionIcon>
+      <Tooltip label="Add notes">
+        <ActionIcon variant="default" size="lg">
+          <IconCirclePlus style={{ width: rem(20) }} stroke={1.5} />
+        </ActionIcon>
+      </Tooltip>
     </ActionIcon.Group>
   );
 }
